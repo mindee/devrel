87e8721e7dc394fdd86ddda3cab9337bfa4e16b5
Frédéric Harper <hi@fred.dev>
2021-09-23 16:45:57 -0400

    add a script section + stars script

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ![mindee-transparent](https://user-images.githubusercontent.com/1170989/130641500-0f72dab7-3b19-4e0c-a267-6bcec9a3ff6e.png)
 
 # Developer Relations at Mindee
+
 This repository serves as a second brain for the Developer Relations (devrel) team at Mindee.
 
 We will slowly add our processes, ideas, [tasks](https://github.com/mindee/devrel/issues) and everything needed to be successful with our community. It's a work-in-progress as we are starting to define the devrel framework, and it's always evolving. By doing so, we hope it will facilitate the start of the journey for the new members of our team, make us accountable, and being transparent to our beloved community of Mindee developers. We also believe other companies could benefit from our expertise and experience of doing developer relations.
@@ -8,4 +9,6 @@ We will slowly add our processes, ideas, [tasks](https://github.com/mindee/devre
 As always, we value constructive feedback. Welcome home!
 
 ## Table of content
+
 - [Ownership](devrel/ownership.md)
+- [Scripts](scripts/scripts.md)
---
+++ scripts/github-stars.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+#
+# Script to get stars from our repositories for our KPI Dashboard
+#
+# You can get your token at https://github.com/settings/tokens
+#
+
+username="fharper"
+token="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"
+org="mindee"
+repos=(
+    "devrel"
+    "doctr"
+    "microsoft-flow-invoice-integration"
+    "mindee-api-python"
+    "mindee-js"
+    "react-mindee-js"
+    "mindee-api-nodejs"
+    "tf-builds"
+    "vue-mindee-js"
+)
+
+###################################
+# Do not modify beyong this point #
+###################################
+
+api_url="https://api.github.com/repos/"
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+printf "\n%s\n" "----------------------"
+echo "| Repositories Stars |"
+printf "%s\n" "----------------------"
+
+# looping the repos
+for repo in "${repos[@]}"
+do
+    stars=$(curl --silent -u $username:$token  "$api_url$org/$repo"  | jq '.stargazers_count')
+    printf "%s\n" "${bold}$repo:${normal} $stars"
+
+    excel="$excel$stars,"
+done
+
+printf "\n\n%s\n" "-------------"
+echo "| Excel CSV |"
+printf "%s\n" "-------------"
+echo "$excel"

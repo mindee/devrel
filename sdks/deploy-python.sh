d2a703f1c1bab05aa1a72ad9b03646395bb96847
Frédéric Harper <hi@fred.dev>
2022-03-04 11:24:03 +0100

    add a script to deploy the Python SDK

---
+++ sdks/deploy-python.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+# To use by Fred
+
+echo "Changing to the Python SDK folder"
+z sdk-python
+echo "Checkout main branch"
+git checkout main
+echo "Pull last updates"
+git pull
+echo "Delete the dist folder"
+rm dist/*
+echo "Creating source distribution"
+python -m build --sdist
+echo "Creating the wheel"
+python -m build --wheel
+echo "Press any key to upload to PyPi"
+read
+python -m twine upload --repository mindee dist/*

01080d1cbe638a8f6a23e4a2ef3a8dfd3c8dd864
Frédéric Harper <hi@fred.dev>
2022-04-25 14:46:10 -0400

    fix extracted data display

--- sdks/testing/node.js/app-doc.js
+++ sdks/testing/node.js/app-doc.js
@@ -13,100 +13,153 @@ mindeeClient.invoice.parse ({
 })
 .then((res) => {
 	// Document prediction level
-	console.log("\n\n");
+	console.log("\n");
 	console.log(chalk.green("Document prediction level"));
 	console.log(res.invoices);
 
 	//Page prediction level
-	console.log("\n\n");
+	console.log("\n");
 	console.log(chalk.green("Page prediction level"));
   	console.log(res.invoices);
 
 	//Full HTTP Response
-	console.log("\n\n");
+	console.log("\n");
 	console.log(chalk.green("Full HTTP Response"));
   	console.log(res.httpResponse);
 
 	//Dates
-	console.log("\n\n");
+	console.log("\n");
 	console.log(chalk.green("Invoice Date"));
-	console.log(res.invoice.invoiceDate);
+	console.log(res.invoice.invoiceDate.value);
 
 	//Due Date
-	console.log("\n\n");
+	console.log("\n");
 	console.log(chalk.green("Due Date"));
-    console.log(res.invoice.dueDate);
+    console.log(res.invoice.dueDate.value);
 
 	//Invoice Number
-	console.log("\n\n");
+	console.log("\n");
 	console.log(chalk.green("Invoice Number"));
-    console.log(res.invoice.invoiceNumber);
+    console.log(res.invoice.invoiceNumber.value);
 
-	//Locale
-	console.log("\n\n");
-	console.log(chalk.green("Locale"));
-    console.log(res.invoice.locale);
+	//Language
+	console.log("\n");
+	console.log(chalk.green("Language"));
+    console.log(res.invoice.locale.language);
+
+	//Country
+	console.log("\n");
+	console.log(chalk.green("Country"));
+    console.log(res.invoice.locale.country);
+
+	//Currency
+	console.log("\n");
+	console.log(chalk.green("Currency"));
+	console.log(res.invoice.locale.currency);
 
 	//Supplier Company Number
-	console.log("\n\n");
+	console.log("\n");
 	console.log(chalk.green("Supplier Company Number"));
-    console.log(res.invoice.companyNumber);
+    console.log(res.invoice.companyNumber.value);
 
 	//Supplier Payment Details
-	console.log("\n\n");
+	console.log("\n");
 	console.log(chalk.green("Supplier Payment Details"));
-    console.log(res.invoice.paymentDetails);
+
+	//Supplier Payment Details - IBAN
+	console.log("\n");
+	console.log(chalk.green("Supplier Payment Details - IBAN"));
+    console.log(res.invoice.paymentDetails.iban);
+
+	//Supplier Payment Details - SWIFT
+	console.log("\n");
+	console.log(chalk.green("Supplier Payment Details - SWIFT"));
+    console.log(res.invoice.paymentDetails.swift);
+
+	//Supplier Payment Details - Routing Number
+	console.log("\n");
+	console.log(chalk.green("Supplier Payment Details - Routing Number"));
+    console.log(res.invoice.paymentDetails.routingNumber);
 
 	//Supplier Name
-	console.log("\n\n");
+	console.log("\n");
 	console.log(chalk.green("Supplier Name"));
-    console.log(res.invoice.supplier);
+    console.log(res.invoice.supplier.value);
 
 	//Taxes
-	console.log("\n\n");
+	console.log("\n");
 	console.log(chalk.green("Taxes"));
-    console.log(res.invoice.taxes);
 
-	//Tests: checklist
-	console.log("\n\n");
+	//Taxes Value
+	console.log("\n");
+	console.log(chalk.green("Value"));
+    console.log(res.invoice.taxes.value);
+	
+	//Taxes Rate
+	console.log("\n");
+	console.log(chalk.green("Rate"));
+    console.log(res.invoice.taxes.rate);
+
+	//Taxes Code
+	console.log("\n");
+	console.log(chalk.green("Code"));
+    console.log(res.invoice.taxes.code);
+
+	//Tests: checklist - Taxes Match Total including Taxes
+	console.log("\n");
 	console.log(chalk.green("Tests: checklist"));
-    console.log(res.invoice.checklist);
+
+	//Taxes Match Total including Taxes
+	console.log("\n");
+	console.log(chalk.green("Taxes Match Total including Taxes"));
+    console.log(res.invoice.checklist.taxesMatchTotalIncl);
+
+	//Taxes Match Total excluding Taxes
+	console.log("\n");
+	console.log(chalk.green("Taxes Match Total excluding Taxes"));
+    console.log(res.invoice.checklist.taxesMatchTotalExcl);
+
+	//Taxes + Total excluding Taxes Match Total including Taxes
+	console.log("\n");
+	console.log(chalk.green("Taxes + Total excluding Taxes Match Total including Taxes"));
+    console.log(res.invoice.checklist.taxesPlusTotalExclMatchTotalIncl);
 
 	//Total amount including taxes
-	console.log("\n\n");
+	console.log("\n");
 	console.log(chalk.green("Total amount including taxes"));
-    console.log(res.invoice.totalIncl);
+    console.log(res.invoice.totalIncl.value);
 
 	//Total amount excluding taxes
-	console.log("\n\n");
+	console.log("\n");
 	console.log(chalk.green("Total amount excluding taxes"));
-    console.log(res.invoice.totalExcl);
+    console.log(res.invoice.totalExcl.value);
 
 	//Total taxes
-	console.log("\n\n");
+	console.log("\n");
 	console.log(chalk.green("Total taxes"));
-    console.log(res.invoice.totalTax);
+    console.log(res.invoice.totalTax.value);
 
 	//Customer name
-	console.log("\n\n");
+	console.log("\n");
 	console.log(chalk.green("Customer name"));
-    console.log(res.invoice.customerName);
+    console.log(res.invoice.customerName.value);
+
+	//Customer Company Registration Type
+	console.log("\n\n");
+	console.log(chalk.green("Customer Company Registration Type"));
+    console.log(res.invoice.customerCompanyRegistration.type);
 
 	//Customer Company Registration
 	console.log("\n\n");
 	console.log(chalk.green("Customer Company Registration"));
-    console.log(res.invoice.customerCompanyRegistration);
+    console.log(res.invoice.customerCompanyRegistration.value);
 
 	//Customer Address
-	console.log("\n\n");
+	console.log("\n");
 	console.log(chalk.green("Customer Address"));
-    console.log(res.invoice.customerAddress);
+    console.log(res.invoice.customerAddress.value);
 
 	//Supplier Address
-	console.log("\n\n");
+	console.log("\n");
 	console.log(chalk.green("Supplier Address"));
-    console.log(res.invoice.supplierAddress);
-})
-.catch((err) => {
-  console.error(err);
-});
+    console.log(res.invoice.supplierAddress.value);
